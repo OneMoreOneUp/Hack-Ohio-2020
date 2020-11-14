34bbcbd43f3f922746ca7bdbec645f52d2a02ce1
Zheng Ji Tan <dunlike2big@hotmail.com>
2020-11-14 12:25:24 -0500

    created moving platform

---
+++ Assets/MovingBlock/moveLeftRight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveLeftRight : MonoBehaviour
+{
+
+    float moveSpeed = 4f, endPos = -10f, startPos = -17f;
+    bool moveRight = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.x < endPos)
+        {
+            moveRight = true;
+        }
+        if (transform.position.x > startPos)
+        {
+            moveRight = false;
+            startPos = -10f;
+            endPos = -17f;
+        }
+
+        if(moveRight){
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime,
+                transform.position.y);
+        }else{
+            transform.position =
+                       new Vector2(transform.position.x - moveSpeed * Time.deltaTime,
+                           transform.position.y);
+        }
+    }
+
+
+}
