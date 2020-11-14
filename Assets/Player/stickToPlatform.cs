303a428221a944d5674e7bfb07ef0655eadfb2d7
Zheng Ji Tan <dunlike2big@hotmail.com>
2020-11-14 16:11:31 -0500

    makes the character stick to the moving platform. solves the floating issue

---
+++ Assets/Player/stickToPlatform.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stickToPlatform : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float dirX, moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        if (Input.GetButtonDown("Jump") && rb.velocity.y == 0)
+        {
+            rb.AddForce(Vector2.up * 1f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Contains("Moveable-Platform (Side A)") || collision.gameObject.name.Contains("Moveable-Platform (Side B)"))
+        {
+            this.transform.parent = collision.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Contains("Moveable-Platform (Side A)") || collision.gameObject.name.Contains("Moveable-Platform (Side B)"))
+        {
+            this.transform.parent = null;
+        }
+    }
+}
