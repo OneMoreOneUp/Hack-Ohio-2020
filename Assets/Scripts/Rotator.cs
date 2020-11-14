51f2ebc603ed6529e94c34da16749dc1992d22e5
Deakon <deakonziegler@gmail.com>
2020-11-14 10:59:12 -0500

    Added a saw blade trap prefab asset with a rotator script.

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    //Public integer to hold the speed of the rotation.
+    public int speed = 45;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Set the sprite's rotation around the Z-axis by multiplying the Vector3, with a value of 45 for Z, by the deltaTime in order to make the rotation smooth and independent to the frame rate.
+        transform.Rotate(new Vector3(0, 0, speed) * Time.deltaTime);
+    }
+}
